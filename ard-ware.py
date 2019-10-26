80c63ddb4615a2dd1f88e813528b1d3db6be4a78
icwhite <54700097+icwhite@users.noreply.github.com>
2019-10-26 15:41:06 -0700

    Update ard-ware.py

--- ard-ware.py
+++ ard-ware.py
@@ -1,11 +1,39 @@
-import pyfirmata
+mport pyfirmata
 import time
 
 board = pyfirmata.Arduino('/dev/cu.usbmodem14101')
-LED_BUILTIN = 13
+buttonPin = 2
+ledPinYellow = 13
+ledPinRed = 12
+ledPinBlue = 11
+
+buttonState = 0
+lightOn = False
+
+## button code
+it = pyfirmata.util.Iterator(board)
+it.start()
+
+board.digital[buttonPin].mode = pyfirmata.INPUT
 
 while True:
-    board.digital[LED_BUILTIN].write(1)
-    time.sleep(1)
-    board.digital[LED_BUILTIN].write(0)
-    time.sleep(1)
+    sw = board.digital[buttonPin].read()
+    if sw is False and not lightOn:
+        lightOn = True
+        board.digital[ledPinYellow].write(1)
+        time.sleep(3)
+        lightOn = False
+    elif lightOn:
+        board.digital[ledPinYellow].write(1)
+    else:
+        board.digital[ledPinYellow].write(0)
+        time.sleep(0.1)
+while True:
+    sw = board.digital[buttonPin].read()
+    #if sw is False:
+        ## send message to backend
+# while True:
+#     board.digital[LED_BUILTIN].write(1)
+#     time.sleep(1)
+#     board.digital[LED_BUILTIN].write(0)
+#     time.sleep(1)
