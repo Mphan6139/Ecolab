68b5e79cc43472c49453cca0bebfbb757d03c68a
Mphan6139 <minhphan54@yahoo.com>
2019-10-26 20:52:31 -0700

    moved back to api from backend

--- classifier.py
+++ classifier.py
@@ -1,6 +1,8 @@
 from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
 from azure.cognitiveservices.vision.customvision.training import CustomVisionTrainingClient
 from azure.cognitiveservices.vision.customvision.training.models import ImageFileCreateEntry
+import cv2 as cv
+
 
 def classify(image_path):
     # Replace with a valid key
@@ -8,7 +10,7 @@ def classify(image_path):
     #prediction_resource_id = "/subscriptions/bad6784e-798e-4bbc-89cf-cb5149375471/resourceGroups/Ecolab(Group)/providers/Microsoft.CognitiveServices/accounts/Ecolab-Prediction"
     training_key = 'd898f79af8f84520994849cf82d4a6be'
     ENDPOINT = "https://ecolab-prediction.cognitiveservices.azure.com/"
-    publish_iteration_name = "EcoIdentify"
+    publish_iteration_name = "Iteration2"
     test_image = image_path
     trainer = CustomVisionTrainingClient(training_key, endpoint=ENDPOINT)
     predictor = CustomVisionPredictionClient(prediction_key, endpoint=ENDPOINT)
@@ -21,7 +23,22 @@ def classify(image_path):
             project.id, publish_iteration_name, image_contents.read())
         # Display the results.
         for prediction in results.predictions:
-            print("\t" + prediction.tag_name +
-                ": {0:.2f}%".format(prediction.probability * 100))
+            return prediction.tag_name
+    #sorted_results = sorted(result,key=lambda x : x[1])
+    #return sorted_results[0][0]
 
-classify('./images/tangerine.jpg')
+
+def take_picture():
+    camera = cv.VideoCapture(0)
+    ret, frame = camera.read()
+    print(ret)
+    cv.imshow("test", frame)
+    img_name = "cv_frame.png"
+    cv.imwrite(img_name, frame)
+    print("{} written!".format(img_name))
+    camera.release()
+    cv.destroyAllWindows()
+
+
+#take_picture()
+print(classify('./cv_frame.png'))
