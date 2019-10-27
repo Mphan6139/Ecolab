e282b33c8a8c83eced706962d24b436e172d3094
icwhite <isadoracw@berkeley.edu>
2019-10-27 00:34:22 -0700

    I started to implement the second button

--- ardware.py
+++ ardware.py
@@ -5,7 +5,7 @@ from classifier import *
 from landing import *
 
 board = pyfirmata.Arduino('/dev/cu.usbmodem14101')
-buttonPin = 2
+buttonDict = {0:2, 1: 5}
 PinDict = {'y':13, 'r': 12, 'b': 11}
 
 buttonState = 0
@@ -15,7 +15,9 @@ lightOn = False
 it = pyfirmata.util.Iterator(board)
 it.start()
 
-board.digital[buttonPin].mode = pyfirmata.INPUT
+board.digital[buttonDict[0]].mode = pyfirmata.INPUT
+board.digital[buttonDict[1]].mode = pyfirmata.INPUT
+
 
 def place_holder():
     #ArdWare.light('y', 1)
@@ -33,10 +35,15 @@ class ArdWare:
         self.loopy()
     def loopy(self):
         while True:
-            self.sw = board.digital[buttonPin].read()
-            if not self.sw and not self.first_run: # button has been pressed
+            self.bp1 = board.digital[buttonDict[0]].read()
+            self.bp2 = board.digital[buttonDict[1]].read()
+            if not self.bp1 and not self.first_run: # button 0 has been pressed
                 self.light('y',1)
                 self.buttonPressed(0)
+                return
+            elif self.bp2 and not self.first_run:
+                self.light('y',1)
+                self.buttonPressed(1)
                 return
             time.sleep(0.1)
             self.first_run = False
