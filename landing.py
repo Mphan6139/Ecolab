974971c4391b0a5ad1ec0d4c3eab40c0fddab90f
Anika Singhania <anikasinghania@Anikas-MacBook-Pro.local>
2019-10-26 20:43:56 -0700

    this is commiting the landing again!

--- landing.py
+++ landing.py
@@ -1,19 +1,38 @@
 users = {}
+recyclable_items = [water_bottle, cans, binder_paper]
+landfill = [boba, popchips]
+compost = [tangerine, used_napkins]
 
 class User:
-    def __init__(self, name, trash, points, history):
+    def __init__(self, name, points=0, history=[]):
         if self not in users:
             self.name = name
             self.points = 0
-            self.trash = trash
             self.history = []
             users[self.name] = points
 
-    def add_points(self, points, history):
+    def get_user(numb):
+        return users[numb]
+
+    def process_result(self, result):
+        if self.trash in recyclable_items:
+            return True
+            self.history.append({self.name, self.points, bin, date})
+        else:
+            return False
+    
+    def change_points(self, points, history):
         #The person throws trash in the correct bin
-        self.points += 1
+        if result == True:
+            self.points += 1
+        else:
+            self.points -= 1
         users[self.name] = self.points
         self.history += self.trash
+
+    
+    
+
         
 
 
